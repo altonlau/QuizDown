9d8151eef2b1abb4e8c245921bfeae3dcb2f9f38
Goverm <gouravev@gmail.com>
2015-10-04 16:11:01 -0400

    Got questions displaying randomly now

--- android/QuizDown/app/src/main/java/com/ag/quizdown/fragment/QuestionFragment.java
+++ android/QuizDown/app/src/main/java/com/ag/quizdown/fragment/QuestionFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ag.quizdown.R;
-import com.ag.quizdown.Utils;
 import com.ag.quizdown.activity.QuestionActivity;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Random;
 
@@ -20,6 +22,10 @@ public class QuestionFragment extends Fragment {
     private static final String JSON_ANSWER = "answer";
 
     private JSONArray mQuestionArray;
+
+    private TextView mTvQuestion;
+
+    private static Random random;
 
     public QuestionFragment() {
         // Required empty public constructor
@@ -35,13 +41,27 @@ public class QuestionFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
+        mTvQuestion = (TextView) getView().findViewById(R.id.question);
+
         mQuestionArray = ((QuestionActivity) getActivity()).getQuestionArray();
 
-        int randNum = Utils.getRandomInt(mQuestionArray.length());
+        int randIndex = getRandomIndex(mQuestionArray.length());
 
+        try {
+            JSONObject jsonQuestionObj = mQuestionArray.getJSONObject(randIndex);
+            String question = jsonQuestionObj.getString(JSON_QUESTION);
+            String answer = jsonQuestionObj.getString(JSON_ANSWER);
 
-
+            mTvQuestion.setText(question);
+        }
+        catch(JSONException e) {
+            e.printStackTrace();
+        }
     }
 
+    public static int getRandomIndex(int maxSize) {
+        random = new Random();
+        return random.nextInt(maxSize);
+    }
 
 }
