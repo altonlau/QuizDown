354b8cce7b3a378bdbdeea6b11a76ece1b47e750
Goverm <gouravev@gmail.com>
2015-10-04 14:09:37 -0400

    Added api to communicate with th the db

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$mysql_host = "mysql17.000webhost.com";
+$mysql_database = "a2291907_qzdown";
+$mysql_user = "a2291907_master";
+$mysql_password = "asd123";
+
+$con=mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+
+if (mysqli_connect_errno($con))
+{
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$rows = array();
+
+$result = mysqli_query($con, "SELECT question, answer FROM `math_questions`");
+while($r = mysqli_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+
+echo json_encode($rows);
+
+mysqli_close($con);
+
+?>
