e61bd9b2205f49ef3db295db903380c1773ca3d8
Goverm <gouravev@gmail.com>
2015-10-04 12:03:06 -0400

    The sheep is now screaming

--- android/QuizDown/app/src/main/java/com/ag/quizdown/sound/Sound.java
+++ android/QuizDown/app/src/main/java/com/ag/quizdown/sound/Sound.java
@@ -3,19 +3,31 @@ package com.ag.quizdown.sound;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 
 import com.ag.quizdown.R;
 
 public class Sound {
 
     private static MediaPlayer mMediaPlayer;// = new MediaPlayer();
-    //private static final Uri sheepUri = Uri.parse("file:///sdcard/music/screaming_sheep.mp3");
+    private static AudioManager audioManager;
+    private static int originalVolume;
 
     public static void screamSheep(Context context) {
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        originalVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
+
         mMediaPlayer = MediaPlayer.create(context, R.raw.screaming_sheep);
-        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
+                mp.release();
+            }
+        });
+
         mMediaPlayer.start();
+
     }
 
 }
