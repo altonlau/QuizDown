561cc41b4987393fa5bb6df0549c49774599b918
Alton Lau <alton@v1020-wn-14-107.campus-dynamic.uwaterloo.ca>
2015-10-04 09:10:40 -0400

    Initial commit ios.

---
+++ ios/QuizDown/QuizDown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizDown
+//
+//  Created by Alton Lau on 2015-10-04.
+//  Copyright © 2015 Alton/Gourave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
